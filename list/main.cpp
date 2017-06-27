a7d6788bc5dc7cdd02bea2ed7ea8cb1802e146a5
iAdanos <iadanos@yandex.com>
2017-06-27 20:28:51 +0300

    LA declaration

--- list/main.cpp
+++ list/main.cpp
@@ -10,11 +10,14 @@ int main(int argc, char **argv)
     char B[]={"sdascnjnjzh"};
 /*    char C[]={"sfhhvhyds"};
     char D[]={"cbbshyhxz"}; */
-    Set *LA=convertDataToList(LA, A, strlen(A));
-    Set *LB=convertDataToList(LB, B, strlen(B));
+    Set *LA=NULL;
+    Set *LB=NULL;
+    Set *LE=NULL;
+    LA=convertDataToList(LA, A, strlen(A));
+    LB=convertDataToList(LB, B, strlen(B));
 /*    Set *LC=convertDataToList(LC, C, strlen(C));
     Set *LD=convertDataToList(LD, D, strlen(D)); */
-    Set *LE=sumSet(LE,LA);
+    LE=sumSet(LE,LA);
     LE=sumSet(LE,LB);
     printSet(LE);
 }
