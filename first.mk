d0e11b8d2a423d8d67cc4fc0a73bcd0832b4c73d
iAdanos <iadanos@yandex.com>
2017-07-04 02:14:34 +0300

    Подсчёт времени
    Механизм подсчёта времени работы был заменён на позволяющий оценить разницу в времени исполнения.

--- first.mk
+++ first.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=first
 ConfigurationName      :=Debug
-WorkspacePath          :=/home/adanos/Documents/ANDS-IV
-ProjectPath            :=/mnt/data/adanos/Documents/ANDS-IV/first
+WorkspacePath          :=D:/ANDS-IV
+ProjectPath            :=D:/ANDS-IV/first
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=Dr. Adanos Diego Flamel
-Date                   :=27/06/17
-CodeLitePath           :=/home/adanos/.codelite
-LinkerName             :=/usr/bin/g++
-SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+User                   :=Adanos
+Date                   :=04/07/2017
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/TDM-GCC-32/bin/g++.exe
+SharedObjectLinkerName :=C:/TDM-GCC-32/bin/g++.exe -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -34,7 +34,9 @@ ArchiveOutputSwitch    :=
 PreprocessOnlySwitch   :=-E
 ObjectsFileList        :="first.txt"
 PCHCompileFlags        :=
-MakeDirCommand         :=mkdir -p
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/TDM-GCC-32/bin/windres.exe
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -47,19 +49,19 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := /usr/bin/ar rcu
-CXX      := /usr/bin/g++
-CC       := /usr/bin/gcc
+AR       := C:/TDM-GCC-32/bin/ar.exe rcu
+CXX      := C:/TDM-GCC-32/bin/g++.exe
+CC       := C:/TDM-GCC-32/bin/gcc.exe
 CXXFLAGS :=  -g -O0 -Wall  -std=c++0x $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall  -std=c++0x $(Preprocessors)
 ASFLAGS  := 
-AS       := /usr/bin/as
+AS       := C:/TDM-GCC-32/bin/as.exe
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=/usr/share/codelite
+CodeLiteDir:=C:\Program Files\CodeLite
 Objects0=$(IntermediateDirectory)/src_bitSetCalculate.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_bitSetPrint.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_bitWordCalculate.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_convertWordtoSet.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_main.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_listPush.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_listPrint.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_listCreateItem.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_convertSettoList.cpp$(ObjectSuffix) $(IntermediateDirectory)/src_listIsMember.cpp$(ObjectSuffix) \
 	$(IntermediateDirectory)/src_listCalculate.cpp$(ObjectSuffix) 
 
@@ -80,11 +82,11 @@ $(OutputFile): $(IntermediateDirectory)/.d $(Objects)
 	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
 
 MakeIntermediateDirs:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@$(MakeDirCommand) "./Debug"
 
 
 $(IntermediateDirectory)/.d:
-	@test -d ./Debug || $(MakeDirCommand) ./Debug
+	@$(MakeDirCommand) "./Debug"
 
 PreBuild:
 
@@ -93,7 +95,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/src_bitSetCalculate.cpp$(ObjectSuffix): src/bitSetCalculate.cpp $(IntermediateDirectory)/src_bitSetCalculate.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/bitSetCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitSetCalculate.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/bitSetCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitSetCalculate.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_bitSetCalculate.cpp$(DependSuffix): src/bitSetCalculate.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_bitSetCalculate.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_bitSetCalculate.cpp$(DependSuffix) -MM src/bitSetCalculate.cpp
 
@@ -101,7 +103,7 @@ $(IntermediateDirectory)/src_bitSetCalculate.cpp$(PreprocessSuffix): src/bitSetC
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_bitSetCalculate.cpp$(PreprocessSuffix) src/bitSetCalculate.cpp
 
 $(IntermediateDirectory)/src_bitSetPrint.cpp$(ObjectSuffix): src/bitSetPrint.cpp $(IntermediateDirectory)/src_bitSetPrint.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/bitSetPrint.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitSetPrint.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/bitSetPrint.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitSetPrint.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_bitSetPrint.cpp$(DependSuffix): src/bitSetPrint.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_bitSetPrint.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_bitSetPrint.cpp$(DependSuffix) -MM src/bitSetPrint.cpp
 
@@ -109,7 +111,7 @@ $(IntermediateDirectory)/src_bitSetPrint.cpp$(PreprocessSuffix): src/bitSetPrint
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_bitSetPrint.cpp$(PreprocessSuffix) src/bitSetPrint.cpp
 
 $(IntermediateDirectory)/src_bitWordCalculate.cpp$(ObjectSuffix): src/bitWordCalculate.cpp $(IntermediateDirectory)/src_bitWordCalculate.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/bitWordCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitWordCalculate.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/bitWordCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_bitWordCalculate.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_bitWordCalculate.cpp$(DependSuffix): src/bitWordCalculate.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_bitWordCalculate.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_bitWordCalculate.cpp$(DependSuffix) -MM src/bitWordCalculate.cpp
 
@@ -117,7 +119,7 @@ $(IntermediateDirectory)/src_bitWordCalculate.cpp$(PreprocessSuffix): src/bitWor
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_bitWordCalculate.cpp$(PreprocessSuffix) src/bitWordCalculate.cpp
 
 $(IntermediateDirectory)/src_convertWordtoSet.cpp$(ObjectSuffix): src/convertWordtoSet.cpp $(IntermediateDirectory)/src_convertWordtoSet.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/convertWordtoSet.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_convertWordtoSet.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/convertWordtoSet.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_convertWordtoSet.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_convertWordtoSet.cpp$(DependSuffix): src/convertWordtoSet.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_convertWordtoSet.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_convertWordtoSet.cpp$(DependSuffix) -MM src/convertWordtoSet.cpp
 
@@ -125,7 +127,7 @@ $(IntermediateDirectory)/src_convertWordtoSet.cpp$(PreprocessSuffix): src/conver
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_convertWordtoSet.cpp$(PreprocessSuffix) src/convertWordtoSet.cpp
 
 $(IntermediateDirectory)/src_main.cpp$(ObjectSuffix): src/main.cpp $(IntermediateDirectory)/src_main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_main.cpp$(DependSuffix): src/main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_main.cpp$(DependSuffix) -MM src/main.cpp
 
@@ -133,7 +135,7 @@ $(IntermediateDirectory)/src_main.cpp$(PreprocessSuffix): src/main.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_main.cpp$(PreprocessSuffix) src/main.cpp
 
 $(IntermediateDirectory)/src_listPush.cpp$(ObjectSuffix): src/listPush.cpp $(IntermediateDirectory)/src_listPush.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/listPush.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listPush.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/listPush.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listPush.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_listPush.cpp$(DependSuffix): src/listPush.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_listPush.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_listPush.cpp$(DependSuffix) -MM src/listPush.cpp
 
@@ -141,7 +143,7 @@ $(IntermediateDirectory)/src_listPush.cpp$(PreprocessSuffix): src/listPush.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_listPush.cpp$(PreprocessSuffix) src/listPush.cpp
 
 $(IntermediateDirectory)/src_listPrint.cpp$(ObjectSuffix): src/listPrint.cpp $(IntermediateDirectory)/src_listPrint.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/listPrint.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listPrint.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/listPrint.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listPrint.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_listPrint.cpp$(DependSuffix): src/listPrint.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_listPrint.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_listPrint.cpp$(DependSuffix) -MM src/listPrint.cpp
 
@@ -149,7 +151,7 @@ $(IntermediateDirectory)/src_listPrint.cpp$(PreprocessSuffix): src/listPrint.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_listPrint.cpp$(PreprocessSuffix) src/listPrint.cpp
 
 $(IntermediateDirectory)/src_listCreateItem.cpp$(ObjectSuffix): src/listCreateItem.cpp $(IntermediateDirectory)/src_listCreateItem.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/listCreateItem.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listCreateItem.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/listCreateItem.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listCreateItem.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_listCreateItem.cpp$(DependSuffix): src/listCreateItem.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_listCreateItem.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_listCreateItem.cpp$(DependSuffix) -MM src/listCreateItem.cpp
 
@@ -157,7 +159,7 @@ $(IntermediateDirectory)/src_listCreateItem.cpp$(PreprocessSuffix): src/listCrea
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_listCreateItem.cpp$(PreprocessSuffix) src/listCreateItem.cpp
 
 $(IntermediateDirectory)/src_convertSettoList.cpp$(ObjectSuffix): src/convertSettoList.cpp $(IntermediateDirectory)/src_convertSettoList.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/convertSettoList.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_convertSettoList.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/convertSettoList.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_convertSettoList.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_convertSettoList.cpp$(DependSuffix): src/convertSettoList.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_convertSettoList.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_convertSettoList.cpp$(DependSuffix) -MM src/convertSettoList.cpp
 
@@ -165,7 +167,7 @@ $(IntermediateDirectory)/src_convertSettoList.cpp$(PreprocessSuffix): src/conver
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_convertSettoList.cpp$(PreprocessSuffix) src/convertSettoList.cpp
 
 $(IntermediateDirectory)/src_listIsMember.cpp$(ObjectSuffix): src/listIsMember.cpp $(IntermediateDirectory)/src_listIsMember.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/listIsMember.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listIsMember.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/listIsMember.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listIsMember.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_listIsMember.cpp$(DependSuffix): src/listIsMember.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_listIsMember.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_listIsMember.cpp$(DependSuffix) -MM src/listIsMember.cpp
 
@@ -173,7 +175,7 @@ $(IntermediateDirectory)/src_listIsMember.cpp$(PreprocessSuffix): src/listIsMemb
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/src_listIsMember.cpp$(PreprocessSuffix) src/listIsMember.cpp
 
 $(IntermediateDirectory)/src_listCalculate.cpp$(ObjectSuffix): src/listCalculate.cpp $(IntermediateDirectory)/src_listCalculate.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "/mnt/data/adanos/Documents/ANDS-IV/first/src/listCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listCalculate.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/ANDS-IV/first/src/listCalculate.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/src_listCalculate.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/src_listCalculate.cpp$(DependSuffix): src/listCalculate.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/src_listCalculate.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/src_listCalculate.cpp$(DependSuffix) -MM src/listCalculate.cpp
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
  ----- */
 
 #include <iostream>
-#include <locale.h>
 #include <time.h>
 
 #include <src/first.h>
@@ -16,11 +15,8 @@ using namespace std;
 int main (int argc, char **argv) {
 
     /* Переменная для подсчёта времени */
-    clock_t runtime;
-
-    /* Конфигурируем поток вывода */
-    cout << fixed;
-    cout.precision(6);
+    clock_t runtime = 0 ;
+    clock_t runtime_total = 0;
 
     srand(time(NULL));
     
@@ -53,24 +49,39 @@ int main (int argc, char **argv) {
     cout << "D: "; listPrint(list[3]); cout << endl << endl;
 
     /* Вычисляем множество E для машинного слова */
-    runtime = clock();
-	bitWord[4] = bitWordCalculate(bitWord[0],bitWord[1],bitWord[2],bitWord[3]);
-    runtime= clock() - runtime;
-    cout << "Set calculated with bit words in " << runtime << " clicks (" << (float)runtime/CLOCKS_PER_SEC << ") seconds." << endl;
+    for (int i = 0; i < 500000; i++) {
+        
+        runtime = clock ();
+        bitWord[4] = bitWordCalculate(bitWord[0],bitWord[1],bitWord[2],bitWord[3]);
+        runtime = clock () - runtime; 
+        runtime_total += runtime ;
+        
+    }
+    cout << "Set calculated with bit words 500000 times in " << runtime_total << " clicks." << endl;
     cout << "Result: "; bitSetPrint(convertWordtoSet(bitWord[4])); cout << endl << endl;
 
     /* Вычисляем множество E для массива бит */
-    runtime = clock();
-    bitSet[4] = bitSetCalculate(bitSet[0],bitSet[1],bitSet[2],bitSet[3]);
-    runtime= clock() - runtime;
-    cout << "Set calculated with bit arrays in " << runtime << " clicks (" << (float)runtime/CLOCKS_PER_SEC << ") seconds." << endl;
+    for (int i = 0; i < 500000; i++) {
+        
+        runtime = clock ();
+        bitSet[4] = bitSetCalculate(bitSet[0],bitSet[1],bitSet[2],bitSet[3]);
+        runtime = clock () - runtime; 
+        runtime_total += runtime ;
+        
+    }
+    cout << "Set calculated with bit arrays 500000 times in " << runtime_total << " clicks." << endl;
     cout << "Result: "; bitSetPrint(bitSet[4]); cout << endl << endl;
 
     /* Вычисляем множество E для списка */
-    runtime = clock();
-    list[4] = listCalculate(list[0],list[1],list[2],list[3]);
-    runtime= clock() - runtime;
-    cout << "Set calculated with stacks in " << runtime << " clicks (" << (float)runtime/CLOCKS_PER_SEC << ") seconds." << endl;
+    for (int i = 0; i < 500000; i++) {
+        
+        runtime = clock ();
+        list[4] = listCalculate(list[0],list[1],list[2],list[3]);
+        runtime = clock () - runtime; 
+        runtime_total += runtime ;
+        
+    }
+    cout << "Set calculated with stacks 500000 times in " << runtime_total << " clicks." << endl;
     cout << endl << "E: "; listPrint(list[4]); cout << endl;
 
     return 0;
